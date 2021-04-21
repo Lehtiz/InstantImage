ab42b8f7eb582498a93163c6e5a363ca0f0f2c42
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-21 11:37:11 +0300

    Basic routing setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import routes constants
+import * as ROUTES from './constants/routes';
+
+// lazy loading dynamically loads on request
+const Login = lazy(() => import('./pages/login.js'));
+
 function App() {
-  return <p>Hello!</p>;
+  return (
+    <Router>
+      {/* Suspense allows us to wait for loading and specify a loading state with fallback */}
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
