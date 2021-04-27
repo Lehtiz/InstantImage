ea581732a75ef32290838d6b623b4fbf663f78bf
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-27 12:12:59 +0300

    Fixed typo on "Sign up"

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
 // Constants for routing
 export const DASHBOARD = '/';
 export const LOGIN = '/login';
-export const SING_UP = '/signup';
+export const SIGN_UP = '/signup';
 export const PROFILE = '/p/:username';
 export const NOT_FOUND = '/not-found';
