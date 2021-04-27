832ca8194074bb68230933356532ea5223e9555a
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-27 11:52:20 +0300

    Firebase config from .env

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -6,7 +6,14 @@ import 'firebase/auth';
 // eslint-disable-next-line no-unused-vars
 import { seedDatabase } from '../seed.js';
 
-const config = {};
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
