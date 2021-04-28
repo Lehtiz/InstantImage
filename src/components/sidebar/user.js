2a0c2b21e9073b8066be5c47c3505549d7f890c8
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-28 12:32:23 +0300

    added user sidebar component functionality

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -3,18 +3,30 @@ import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import { memo } from 'react';
 
+// Show a skeleton whilst loading, and if no user defined
 const User = ({ username, fullName }) =>
   !username || !fullName ? (
     <Skeleton count={1} height={61} />
   ) : (
-    <Link to={`/p/${username}`} className="grid">
-      <p>{username}</p>
+    <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+      <div className="flex items-center justify-between col-spaln-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt={`${username} profile`}
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
     </Link>
   );
 
 export default User;
 
+// require props with types for functionality
 User.propTypes = {
-  username: PropTypes.string,
-  fullName: PropTypes.string
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired
 };
