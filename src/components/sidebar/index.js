c0af14cef025a28c38dd538a5237b50e3d7c041c
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-29 11:48:10 +0300

    Working profile uggestions

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -4,14 +4,14 @@ import Suggestions from './suggestions';
 
 export default function Sidebar() {
   const {
-    user: { fullName, username, userId }
+    user: { docId, fullName, username, userId, following }
   } = useUser();
 
   return (
     <div className="p-4">
       <User username={username} fullName={fullName} />
       {/* Suggestions need userId so we know who we're already following */}
-      <Suggestions userId={userId} />
+      <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
     </div>
   );
 }
--- src/components/sidebar/suggestions.js
+++ src/components/sidebar/suggestions.js
@@ -1,3 +1,49 @@
-export default function Suggestions() {
-  return <p>I am suggestions</p>;
+/* eslint-disable no-nested-ternary */
+import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Skeleton from 'react-loading-skeleton';
+import { getSuggestedProfiles } from '../../services/firebase';
+import SuggestedProfile from './suggested-profile';
+
+export default function Suggestions({ userId, following, loggedInUserDocId }) {
+  const [profiles, setProfiles] = useState(null);
+  // for async functions in react use useEffect, also dependent on userId
+  useEffect(() => {
+    async function suggestedProfiles() {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+    }
+    // only call function if there is a userId present
+    if (userId) {
+      suggestedProfiles();
+    }
+  }, [userId]);
+
+  return !profiles ? (
+    <Skeleton count={1} height={150} className="mt-5" />
+  ) : profiles.length > 0 ? (
+    <div className="rounded flex flex-col">
+      <div className="text-sm items-center align-items justify-between mb-2">
+        <p className="font-bold text-gray-base">Suggestions for you</p>
+      </div>
+      <div className="mt-4 grid gap-5">
+        {profiles.map((profile) => (
+          <SuggestedProfile
+            key={profile.docId}
+            profileDocId={profile.docId}
+            username={profile.username}
+            profileId={profile.userId}
+            userId={userId}
+            loggedInUserDocId={loggedInUserDocId}
+          />
+        ))}
+      </div>
+    </div>
+  ) : null; // render null when we dont want to render anything
 }
+
+Suggestions.propTypes = {
+  userId: PropTypes.string,
+  following: PropTypes.array,
+  loggedInUserDocId: PropTypes.string
+};
