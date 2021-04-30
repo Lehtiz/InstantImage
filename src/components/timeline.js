a6516e043246abe663d550a2a76ef6644ebbe35e
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-30 15:05:13 +0300

    Finished timeline and post component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import Skeleton from 'react-loading-skeleton';
 import usePhotos from '../hooks/use-photos';
+import Post from './post';
 
 export default function Timeline() {
   // Get the logged in users photos (userContext)
@@ -9,12 +10,10 @@ export default function Timeline() {
   return (
     <div className="container col-span-2">
       {!photos ? (
-        <>
-          <Skeleton count={4} width={640} height={500} className="mb-5" />
-        </>
+        <Skeleton count={4} width={640} height={500} className="mb-5" />
       ) : photos?.length > 0 ? (
         // If we have photos render them
-        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+        photos.map((content) => <Post key={content.docId} content={content} />)
       ) : (
         // if the user has no photos, tell them to create some photos
         <p className="text-center text-2xl">Follow people to see photos</p>
