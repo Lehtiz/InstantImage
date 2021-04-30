3eebaa6373043fc6431c29e9195ae3394abf862f
Mikko Lehtosaari <lehtiz@gmail.com>
2021-04-30 11:04:57 +0300

    Added basic timeline functionality

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,7 +1,24 @@
+/* eslint-disable no-nested-ternary */
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
+  // Get the logged in users photos (userContext)
+  const { photos } = usePhotos();
+
   return (
     <div className="container col-span-2">
-      <p>I am the timeline</p>
+      {!photos ? (
+        <>
+          <Skeleton count={4} width={640} height={500} className="mb-5" />
+        </>
+      ) : photos?.length > 0 ? (
+        // If we have photos render them
+        photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>)
+      ) : (
+        // if the user has no photos, tell them to create some photos
+        <p className="text-center text-2xl">Follow people to see photos</p>
+      )}
     </div>
   );
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -63,3 +63,33 @@ export async function updateFollowedUserFollowers(
         : FieldValue.arrayUnion(loggedInUserDocId) // Add following if not followed already
     });
 }
+
+export async function getPhotos(userId, following) {
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get();
+
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...photo.data(),
+    docId: photo.id
+  }));
+
+  const photosWithUserDetails = await Promise.all(
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false;
+      if (photo.likes.includes(userId)) {
+        userLikedPhoto = true;
+      }
+      // Find user that the photo belongs to
+      const user = await getUserByUserId(photo.userId);
+      // Gets name from firebases return array
+      const { username } = user[0];
+
+      return { username, ...photo, userLikedPhoto };
+    })
+  );
+
+  return photosWithUserDetails;
+}
