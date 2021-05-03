fab3aae4d1f60fc57c894a6b4aa007924d723f5e
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-03 16:47:17 +0300

    Added comment pagination

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -6,27 +6,37 @@ import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
   const [comments, setComments] = useState(allComments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+
+  const showMoreComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
+
   return (
     <>
       <div className="p-4 pt-1 pb-4">
-        {
-          // If there are too many comments show a link to thread
-          comments.length >= 3 && (
-            // TODO: add link to photos
-            <p className="text-sm text-gray-base mb-1 cursor-pointer">View all comments</p>
-          )
-        }
-        {
-          // Show up to 3 latest comment before cut off
-          comments.slice(-3).map((item) => (
-            <p key={`${item.comment}-${item.displayName}`} className="mb-1">
-              <Link to={`/p/${item.displayName}`}>
-                <span className="mr-1 font-bold">{item.displayName}</span>
-              </Link>
-              <span>{item.comment}</span>
-            </p>
-          ))
-        }
+        {comments.length >= 3 && commentsSlice < comments.length && (
+          <button
+            className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+            type="button"
+            onClick={showMoreComments}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                showMoreComments();
+              }
+            }}
+          >
+            View more comments
+          </button>
+        )}
+        {comments.slice(-commentsSlice).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
         <p className="text-gray-base uppercase text-xs mt-2">
           {
             // formatDistance enables to format the unix epoch time stamp to a human readable format
