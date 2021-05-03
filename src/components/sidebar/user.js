8f1fe38119afcfe9c8e153f49bca1b05a3fbea78
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-03 23:14:40 +0300

    Default pic logic

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Skeleton from 'react-loading-skeleton';
 import { memo } from 'react';
+import { DEFAULT_IMAGE_PATH } from '../../constants/paths';
 
 // Show a skeleton whilst loading, and if no user defined
 const User = ({ username, fullName }) =>
@@ -14,6 +15,9 @@ const User = ({ username, fullName }) =>
           className="rounded-full w-16 flex mr-3"
           src={`/images/avatars/${username}.jpg`}
           alt={`${username} profile`}
+          onError={(e) => {
+            e.target.src = DEFAULT_IMAGE_PATH;
+          }}
         />
       </div>
       <div className="col-span-3">
