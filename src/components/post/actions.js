07908fa0dd3f8b0b12d946b572ac6c85c8c94958
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-03 16:47:26 +0300

    Layout tweaks

--- src/components/post/actions.js
+++ src/components/post/actions.js
@@ -28,7 +28,7 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
 
   return (
     <>
-      <div className="flex justify-between p-4">
+      <div className="flex justify-between px-4 py-0">
         <div className="flex">
           <svg
             onClick={handleToggleLiked}
@@ -60,7 +60,7 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
                 handleFocus();
               }
             }}
-            className="w-8 text-black-light select-none cursor-pointer"
+            className="w-8 mr-4 text-black-light select-none cursor-pointer"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
@@ -73,12 +73,11 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
               strokeWidth={2}
               d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
             />
-            >
           </svg>
+          <div className="py-4 items">
+            <p className=" font-bold">{likes === 1 ? `${likes} like` : `${likes} likes`}</p>
+          </div>
         </div>
-      </div>
-      <div className="p-4 py-0">
-        <p className=" font-bold">{likes === 1 ? `${likes} like` : `${likes} likes`}</p>
       </div>
     </>
   );
