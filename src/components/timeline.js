f3d49bd356e0479277541a426e86c218675517bc
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-04 10:12:18 +0300

    Fixed a bug with new user having no followings

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -12,15 +12,25 @@ export default function Timeline() {
 
   return (
     <div className="container col-span-2">
-      {!photos ? (
-        <Skeleton count={4} width="100%" height="270%" className="mb-5" />
-      ) : photos.length > 0 ? (
-        // If we have photos render them
-        photos.map((content) => <Post key={content.docId} content={content} />)
-      ) : (
-        // if the user has no photos, tell them to create some photos
-        <p className="text-center text-2xl">Follow people to see photos</p>
-      )}
+      {
+        // Show loading while we don't have photos or user data
+        !photos && user.following === undefined ? (
+          // No photo and user data skeleton
+          <Skeleton count={4} width="100%" height="270%" className="mb-5" />
+        ) : // User is following, and there are photos to show
+        user.following !== undefined && photos && photos.length > 0 ? (
+          photos.map((content) => <Post key={content.docId} content={content} />)
+        ) : // User is following but there are no posts
+        user.following !== undefined && photos && photos.length === 0 ? (
+          <p className="text-center text-2xl">People you follow have no posts</p>
+        ) : // User is not following anyone
+        user && user.following.length === 0 ? (
+          <p className="text-center text-2xl">Follow people to see photos</p>
+        ) : (
+          // fallback skeleton
+          <Skeleton count={4} width="100%" height="270%" className="mb-5" />
+        )
+      }
     </div>
   );
 }
